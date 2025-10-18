d414e21e8172c4fc01e3884e4f5a683567412b8c
Ricardo-Patino <rickpatinor@gmail.com>
2025-10-18 08:06:20 -0600

    debug

--- Hotel 2/app.py
+++ Hotel 2/app.py
@@ -3382,3 +3382,6 @@ if __name__ == "__main__":
     except Exception:
         pass
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+    if __name__ == "__main__":
+        app.run(debug=True)
