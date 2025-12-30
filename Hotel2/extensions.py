ab28320539b4189196b7e6f3daa17e3adac1fde7
farce19 <fabarcsa@hotmail.com>
2025-12-30 17:36:35 -0600

    Solución registrar usuarios 2

--- Hotel2/extensions.py
+++ Hotel2/extensions.py
@@ -2,6 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
 
-db = SQLAlchemy()
+db = SQLAlchemy(engine_options={"connect_args": {"ssl": {"ca": "ca-certificate.crt"}}})
 migrate = Migrate()
 mail = Mail()
